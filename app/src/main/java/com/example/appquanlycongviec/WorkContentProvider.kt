0fff17d88b0aa2ef2afa2ff3968f3589bcef2701
QuanBVM1 <bquan397@gmail.com>
2023-09-18 22:39:23 +0700

    "Update"

--- app/src/main/java/com/example/appquanlycongviec/WorkContentProvider.kt
+++ app/src/main/java/com/example/appquanlycongviec/WorkContentProvider.kt
@@ -4,14 +4,16 @@ import android.content.*
 import android.database.Cursor
 import android.net.Uri
 import com.example.appquanlycongviec.ListWork.Companion.sqLiteHelper
-import com.example.appquanlycongviec.MainActivity.Companion.PROVIDER
-import com.example.appquanlycongviec.MainActivity.Companion.TABLE
-import com.example.appquanlycongviec.MainActivity.Companion.myUricode
-import com.example.appquanlycongviec.MainActivity.Companion.uriMatcher
 import java.lang.IllegalArgumentException
 
 class WorkContentProvider : ContentProvider() {
+    var myUricode = 1
+    val PROVIDER : String = "com.example.appquanlycongviec.WorkContentProvider"
+    val TABLE : String = "tbl_work"
+    var uriMatcher : UriMatcher? = null
     override fun onCreate(): Boolean {
+        uriMatcher = UriMatcher(UriMatcher.NO_MATCH)
+        uriMatcher!!.addURI(PROVIDER, TABLE, myUricode)
         return true
     }
     override fun query(
@@ -22,10 +24,10 @@ class WorkContentProvider : ContentProvider() {
         p4: String?
     ): Cursor? {
         var cursor : Cursor? = null
-        when (uriMatcher.match(p0)) {
+        when (uriMatcher!!.match(p0)) {
             myUricode -> {
                 cursor = sqLiteHelper.writableDatabase.query(TABLE, p1, p2, p3, null, null, p4)
-                getContext()!!.getContentResolver().notifyChange(p0, null)
+                context!!.getContentResolver().notifyChange(p0, null)
             }
             else -> {throw IllegalArgumentException("Unknown Uri: " + p0.toString())}
         }
@@ -33,7 +35,7 @@ class WorkContentProvider : ContentProvider() {
     }
 
     override fun getType(p0: Uri): String? {
-        when (uriMatcher.match(p0)) {
+        when (uriMatcher!!.match(p0)) {
             myUricode -> {
                 return PROVIDER + "/" + TABLE
             }
@@ -44,11 +46,11 @@ class WorkContentProvider : ContentProvider() {
     override fun insert(p0: Uri, p1: ContentValues?): Uri? {
         var rowId : Long = 0
         var uri : Uri? = null
-        when (uriMatcher.match(p0)) {
+        when (uriMatcher!!.match(p0)) {
             myUricode -> {
                 rowId = sqLiteHelper.writableDatabase.insert(TABLE, null, p1)
                 uri = ContentUris.withAppendedId(p0, rowId)
-                MainActivity.context.contentResolver.notifyChange(uri, null)
+                context!!.contentResolver.notifyChange(uri, null)
                 return uri
             }
             else -> {throw IllegalArgumentException("Unknown Uri: " + p0.toString())}
@@ -57,7 +59,7 @@ class WorkContentProvider : ContentProvider() {
 
     override fun delete(p0: Uri, p1: String?, p2: Array<out String>?): Int {
         var rowId : Int = 0
-        when (uriMatcher.match(p0)) {
+        when (uriMatcher!!.match(p0)) {
             myUricode -> {
                 rowId = sqLiteHelper.writableDatabase.delete(TABLE, p1, p2)
                 context!!.contentResolver.notifyChange(p0, null)
@@ -69,7 +71,7 @@ class WorkContentProvider : ContentProvider() {
 
     override fun update(p0: Uri, p1: ContentValues?, p2: String?, p3: Array<out String>?): Int {
         var rowId : Int = 0
-        when (uriMatcher.match(p0)) {
+        when (uriMatcher!!.match(p0)) {
             myUricode -> {
                 rowId = sqLiteHelper.writableDatabase.update(TABLE, p1, p2, p3)
                 context!!.contentResolver.notifyChange(p0, null)
